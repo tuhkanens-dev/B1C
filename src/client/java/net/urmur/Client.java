29550dddd184b6ea9215f7c3527cd9a43ad5922d
TuhkanenS <tuhkanenssss@gmail.com>
2026-04-09 18:26:53 +0300

    Initial commit

---
+++ src/client/java/net/urmur/Client.java
@@ -0,0 +1,10 @@
+package net.urmur;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/urmur/Main.java
@@ -0,0 +1,10 @@
+package net.urmur;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
