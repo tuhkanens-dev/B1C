87111ec8dc598f46e6b2bfbed21f3b4b155ec6d8
TuhkanenS <tuhkanenssss@gmail.com>
2026-04-09 21:13:35 +0300

    Join Message

--- src/client/java/net/dev/DataGenerator.java
+++ src/client/java/net/dev/DataGenerator.java
@@ -1,4 +1,4 @@
-package net.urmur;
+package net.dev;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
--- src/main/java/net/dev/Main.java
+++ src/main/java/net/dev/Main.java
@@ -1,10 +1,17 @@
 package net.dev;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.text.Text;
 
 public class Main implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server ) -> {
+            String playerName = handler.getPlayer().getName().getString();
+            Text message = Text.literal(playerName);
+            handler.getPlayer().sendMessage(message, false);
+        });
     }
 }
